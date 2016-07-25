5c4818b857a12361aa8c4b5f1ee16cfc9e5fc7b6
ivanbrennan <ivan.brennan@gmail.com>
2016-07-24 22:59:16 -0400

    some color themes

--- init.el
+++ init.el
@@ -37,6 +37,7 @@
 (setq eshell-directory-name (concat ivan-cache-directory "eshell/"))
 
 (setq custom-theme-directory (concat user-emacs-directory "themes/"))
+(load-theme 'paper t)
 
 (setq initial-scratch-message "")
 (setq inhibit-startup-screen t)
