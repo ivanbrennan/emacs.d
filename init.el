8df877345b816128086277cf0e19b48ee47e7e8f
ivanbrennan <ivan.brennan@gmail.com>
2016-07-30 22:38:57 -0400

    more color tweaks, customizations

--- init.el
+++ init.el
@@ -36,7 +36,7 @@
 
 (setq custom-theme-directory (concat user-emacs-directory "themes/"))
 (add-to-list 'load-path custom-theme-directory)
-(load-theme 'sanityinc-tomorrow-night t)
+(load-theme 'sanityinc-tomorrow-bright t)
 
 ;;(set-frame-parameter (selected-frame) 'alpha '(<active> . <inactive>))
 (set-frame-parameter (selected-frame) 'alpha '(98 . 88))
@@ -153,3 +153,9 @@
 
 (require 'evil)
 ;(evil-mode t)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#151719" :foreground "#eaeaea" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 161 :width normal :foundry "nil" :family "Source Code Pro")))))
