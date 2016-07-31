3536f612b7810d1cb55c6aa160306d5354f357c1
ivanbrennan <ivan.brennan@gmail.com>
2016-07-31 10:13:23 -0400

    slate-theme

--- init.el
+++ init.el
@@ -36,7 +36,7 @@
 
 (setq custom-theme-directory (concat user-emacs-directory "themes/"))
 (add-to-list 'load-path custom-theme-directory)
-(load-theme 'sanityinc-tomorrow-bright t)
+(load-theme 'slate t)
 
 ;;(set-frame-parameter (selected-frame) 'alpha '(<active> . <inactive>))
 (set-frame-parameter (selected-frame) 'alpha '(98 . 88))
@@ -46,7 +46,6 @@
 (setq inhibit-startup-screen t)
 (tool-bar-mode -1)
 (tooltip-mode 0)
-;(set-cursor-color "#eeeeee")
 
 (setq whitespace-style
       (quote (face
@@ -155,9 +154,3 @@
 
 (require 'evil)
 ;(evil-mode t)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#151719" :foreground "#eaeaea" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 161 :width normal :foundry "nil" :family "Source Code Pro")))))
