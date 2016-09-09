dabe6d80c239e5c87b4b5618c03c198b0bea2d53
ivanbrennan <ivan.brennan@gmail.com>
2016-09-09 18:06:40 -0400

    company-mode

--- init.el
+++ init.el
@@ -269,6 +269,11 @@
     (global-evil-leader-mode))
   (evil-mode))
 
+(use-package company
+  :bind (:map company-active-map
+        ("C-n" . company-select-next)
+        ("C-p" . company-select-previous)))
+
 (use-package ag
   :ensure t
   :commands (ag
