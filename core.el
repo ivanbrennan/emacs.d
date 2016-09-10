3766fc5ecdbf0b7494693adc42abe7508cfd6b74
ivanbrennan <ivan.brennan@gmail.com>
2016-09-10 18:31:57 -0400

    reverse howto title order
    Move newest entries to top to mirror the issues page on GitHub.

--- core.el
+++ core.el
@@ -161,8 +161,8 @@
 (defun ivan/create-non-existent-directory ()
   (let ((parent-directory (file-name-directory buffer-file-name)))
     (when (and (not (file-exists-p parent-directory))
-               (y-or-n-p (format "Directory `%s' does not exist! Create it?" parent-directory)))
-      (make-directory parent-directory t))))
+               (y-or-n-p (format "Directory ‘%s’ does not exist! Create it?" parent-directory)))
+      (make-directory parent-directory :mkdir_p))))
 
 (add-to-list 'find-file-not-found-functions 'ivan/create-non-existent-directory)
 
