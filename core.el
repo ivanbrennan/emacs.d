501cd66c5d2703c6d642071edbbf145a7027da81
ivanbrennan <ivan.brennan@gmail.com>
2016-09-21 09:44:26 -0400

    leader C-b list-buffers

--- core.el
+++ core.el
@@ -305,6 +305,7 @@
     (evil-leader/set-key "N"     'ivan/toggle-narrowing)
     (evil-leader/set-key ","     'evil-window-next)
     (evil-leader/set-key "\\"    'ivan/cycle-theme)
+    (evil-leader/set-key "C-b"   'list-buffers)
     (evil-leader/set-key "f a"   'find-alternate-file)
     (evil-leader/set-key "f j"   'dired-jump)
     (evil-leader/set-key "f o"   'find-file)
