2fc5e0838d190d1af6d3acfc91db2653f12bdcf2
ivanbrennan <ivan.brennan@gmail.com>
2016-09-20 21:43:06 -0400

    let dired-up-directory make new buffers
    While it's nice to keep a tidy buffer list, it's even nicer to pop up
    and down the directory structure while maintaining the cursor position
    at each entry point, and allowing dired to generate new buffers
    facilitates this.

--- config/dired-config.el
+++ config/dired-config.el
@@ -1,11 +1,4 @@
 (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
 (put 'dired-find-file :advertised-binding "f")
 
-(defun ivan/up-directory ()
-  (interactive)
-  (find-alternate-file ".."))
-
-(define-key dired-mode-map "^" 'ivan/up-directory)
-(evil-define-key 'normal dired-mode-map "_" 'ivan/up-directory)
-
 (provide 'dired-config)
