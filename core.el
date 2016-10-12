00dc1c3cc3756b29a62037a5ab0140666bfb0413
ivanbrennan <ivan.brennan@gmail.com>
2016-10-12 08:48:13 -0400

    space g s magit-status
    I'm going back to gs for this, as it'll keep things more similar to my
    gst alias in bash and I find key sequences with repeated keys (gg)
    annoying to type.

--- core.el
+++ core.el
@@ -304,7 +304,7 @@
     "f s"        'save-buffer
     "f w"        'write-file
     "g b"        'magit-blame
-    "g g"        'magit-status
+    "g s"        'magit-status
     "l"          'evil-switch-to-windows-last-buffer
     "m e b"      'eval-buffer
     "m e f"      'eval-defun
