0a09fea439218addadccdf5bfa6703aaa9bd6dba
ivanbrennan <ivan.brennan@gmail.com>
2017-01-15 10:16:33 -0500

    update safe-local-eval-forms

--- core.el
+++ core.el
@@ -1515,4 +1515,6 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
  )
 
 ;;; temporary, for the refactoring demo
-(add-to-list 'safe-local-eval-forms '(load-file "refactoring-demo-environment.el"))
+(add-to-list 'safe-local-eval-forms '(load (expand-file-name "refactoring-demo-environment")
+                                           nil
+                                           'no-message))
