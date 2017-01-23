f0c7f514a9b30073fc1ff000a3c322a204548e81
ivanbrennan <ivan.brennan@gmail.com>
2017-01-23 00:46:21 -0500

    powerline progress

--- core.el
+++ core.el
@@ -1162,19 +1162,70 @@
         powerline-display-buffer-size nil
         powerline-display-mule-info   nil
         powerline-gui-use-vcs-glyph   t)
-  (powerline-default-theme))
+
+  (defface ivan/active-hud1 '((t (:background "#B3B3B3")))
+    "HUD face 1."
+    :group 'powerline)
+
+  (defun ivan/line-theme ()
+    "Setup a custom mode-line."
+    (interactive)
+    (setq-default mode-line-format
+                  '("%e"
+                    (:eval
+                     (let* ((active (powerline-selected-window-active))
+                            (mode-line-buffer-id (if active 'mode-line-buffer-id 'mode-line-buffer-id-inactive))
+                            (mode-line (if active 'mode-line 'mode-line-inactive))
+                            (face1 (if active 'powerline-active1 'powerline-inactive1))
+                            (face2 (if active 'powerline-active2 'powerline-inactive2))
+                            (hud-fg (if active 'ivan/active-hud1 face2))
+                            (separator-left (intern (format "powerline-%s-%s"
+                                                            (powerline-current-separator)
+                                                            (car powerline-default-separator-dir))))
+                            (separator-right (intern (format "powerline-%s-%s"
+                                                             (powerline-current-separator)
+                                                             (cdr powerline-default-separator-dir))))
+                            (lhs (append (when evil-mode
+                                           (list (powerline-raw evil-mode-line-tag face1)
+                                                 (powerline-raw " " face1)
+                                                 (funcall separator-left face1 face2)))
+                                         (list (powerline-raw "%*" face2 'l)
+                                               (powerline-buffer-id mode-line-buffer-id 'l)
+                                               (powerline-raw " ")
+                                               (funcall separator-right face2 face1)
+                                               (powerline-major-mode face1 'l)
+                                               (powerline-process face1)
+                                               (powerline-narrow face1 'l)
+                                               (powerline-raw " " face1)
+                                               (funcall separator-left face1 face2)
+                                               (powerline-vc face2 'r))))
+                            (rhs (list (powerline-raw global-mode-string face2 'r)
+                                       (funcall separator-right face2 face1)
+                                       (powerline-raw "%4l" face1 'l)
+                                       (powerline-raw ":" face1 'l)
+                                       (powerline-raw "%3c" face1 'r)
+                                       (funcall separator-left face1 face2)
+                                       (powerline-raw " ")
+                                       (powerline-raw "%6p" face2 'r)
+                                       (powerline-hud hud-fg face1))))
+                       (concat (powerline-render lhs)
+                               (powerline-fill face2 (powerline-width rhs))
+                               (powerline-render rhs)))))))
+
+  (ivan/line-theme)
+  )
+
+(defadvice vc-mode-line (after strip-backend () activate)
+  (when (stringp vc-mode)
+    (setq vc-mode
+          (replace-regexp-in-string "^ Git." " " vc-mode))))
 
 (use-package spaceline-config
   :disabled t
   :ensure spaceline
   :config
   (spaceline-spacemacs-theme)
-  (setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state))
-
-(defadvice vc-mode-line (after strip-backend () activate)
-  (when (stringp vc-mode)
-    (let ((gitlogo (replace-regexp-in-string "^ Git." " " vc-mode)))
-          (setq vc-mode gitlogo))))
+  )
 
 (use-package org-bullets
   :ensure t
