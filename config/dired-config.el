b969fc172bd8723aec023ccb76fa8bfafdd688a0
ivanbrennan <ivan.brennan@gmail.com>
2017-01-29 16:41:14 -0500

    dired: default to long-format
    The short format is nice for neotree, but in dired I'm starting to
    prefer the long format listings.

--- config/dired-config.el
+++ config/dired-config.el
@@ -1,6 +1,5 @@
 (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
 (define-key dired-mode-map (kbd "<return>") 'dired-find-alternate-file)
 (put 'dired-find-file :advertised-binding "f")
-(add-hook 'dired-mode-hook #'dired-hide-details-mode)
 
 (provide 'dired-config)
