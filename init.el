688890844a4d080a94a627e8177e0e720f7dc848
ivanbrennan <ivan.brennan@gmail.com>
2017-02-06 08:08:55 -0500

    init server

--- init.el
+++ init.el
@@ -2,4 +2,9 @@
       (gc-cons-percentage 0.6)
       (file-name-handler-alist nil))
 
-  (load-file (concat user-emacs-directory "core.el")))
+  (load-file (concat user-emacs-directory "core.el"))
+
+  (when (display-graphic-p)
+    (require 'server)
+    (unless (server-running-p)
+      (server-start))))
