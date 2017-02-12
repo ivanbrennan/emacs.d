f0f4d8afc805233088051113014e757d522ccf45
ivanbrennan <ivan.brennan@gmail.com>
2017-02-12 12:53:33 -0500

    put horizontal scroll behind modifier key
    This way I can casually scroll up/down with the trackpad without
    accidentally scrolling sideways.

--- core.el
+++ core.el
@@ -301,8 +301,10 @@
 
 (defvar ivan/hscroll-minor-mode-map
   (let ((map (make-sparse-keymap)))
-    (define-key map [wheel-right] #'ivan/scroll-left)
-    (define-key map [wheel-left] #'ivan/scroll-right)
+    (define-key map [C-wheel-right] #'ivan/scroll-left)
+    (define-key map [S-wheel-right] #'ivan/scroll-left)
+    (define-key map [C-wheel-left]  #'ivan/scroll-right)
+    (define-key map [S-wheel-left]  #'ivan/scroll-right)
     map)
   "ivan/hscroll-minor-mode keymap.")
 
