8ff214767a27da601d553c6e7fe28b20d1801c37
ivanbrennan <ivan.brennan@gmail.com>
2017-02-19 12:18:37 -0500

    reclaim & rethink "/" in ibuffer and evil
    Reclaim evil-search functionality on "/" in ibuffer-mode, and move the
    ibuffer filtering functions into a leader map.

--- config/ibuffer-config.el
+++ config/ibuffer-config.el
@@ -1,3 +1,8 @@
+(bind-map-for-mode-inherit ivan/ibuffer-leader-map ivan/leader-map
+  :major-modes (ibuffer-mode)
+  :bindings
+  ("/" (lookup-key ibuffer-mode-map "/")))
+
 (evil-set-initial-state 'ibuffer-mode 'motion)
 
 (evil-define-key 'motion ibuffer-mode-map
@@ -23,35 +28,8 @@
   "b" 'ibuffer-bury-buffer
   (kbd ",") 'ibuffer-toggle-sorting-mode
 
-  (kbd "/ m") 'ibuffer-filter-by-used-mode
-  (kbd "/ M") 'ibuffer-filter-by-derived-mode
-  (kbd "/ n") 'ibuffer-filter-by-name
-  (kbd "/ c") 'ibuffer-filter-by-content
-  (kbd "/ e") 'ibuffer-filter-by-predicate
-  (kbd "/ f") 'ibuffer-filter-by-filename
-  (kbd "/ >") 'ibuffer-filter-by-size-gt
-  (kbd "/ <") 'ibuffer-filter-by-size-lt
-  (kbd "/ r") 'ibuffer-switch-to-saved-filters
-  (kbd "/ a") 'ibuffer-add-saved-filters
-  (kbd "/ x") 'ibuffer-delete-saved-filters
-  (kbd "/ d") 'ibuffer-decompose-filter
-  (kbd "/ s") 'ibuffer-save-filters
-  (kbd "/ p") 'ibuffer-pop-filter
-  (kbd "/ !") 'ibuffer-negate-filter
-  (kbd "/ t") 'ibuffer-exchange-filters
-  (kbd "/ TAB") 'ibuffer-exchange-filters
-  (kbd "/ o") 'ibuffer-or-filter
-  (kbd "/ g") 'ibuffer-filters-to-filter-group
-  (kbd "/ P") 'ibuffer-pop-filter-group
-  (kbd "/ D") 'ibuffer-decompose-filter-group
-  (kbd "/ /") 'ibuffer-filter-disable
-
   "\t" 'ibuffer-forward-filter-group
   (kbd "p") 'ibuffer-yank
-  (kbd "/ S") 'ibuffer-save-filter-groups
-  (kbd "/ R") 'ibuffer-switch-to-saved-filter-groups
-  (kbd "/ X") 'ibuffer-delete-saved-filter-groups
-  (kbd "/ \\") 'ibuffer-clear-filter-groups
 
   (kbd "% n") 'ibuffer-mark-by-name-regexp
   (kbd "% m") 'ibuffer-mark-by-mode-regexp
