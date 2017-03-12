bb8da866a72b2d0ac09f243139b7bedeed7aca9b
ivanbrennan <ivan.brennan@gmail.com>
2017-03-12 09:08:34 -0400

    fix elixir-theme syntax error

--- themes/elixir-theme.el
+++ themes/elixir-theme.el
@@ -269,7 +269,7 @@ Also bind `class' to ((class color) (min-colors 89))."
    `(popup-tip-face ((t (:background ,bloodred :foreground ,leaf))))
    `(popup-scroll-bar-foreground-face ((t (:background ,ink))))
    `(popup-scroll-bar-background-face ((t (:background ,paper))))
-   `(popup-isearch-match ((t (:background ,marooned :foreground aqua))))
+   `(popup-isearch-match ((t (:background ,marooned :foreground ,aqua))))
 ;;;;; avy
    `(avy-background-face
      ((t (:foreground ,ink :background ,paper :inverse-video nil))))
