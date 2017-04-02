8ec66934bdb54bed524478e5ee641d381d4c8dc5
ivanbrennan <ivan.brennan@gmail.com>
2017-04-02 11:37:59 -0400

    SPC c q with-editor-cancel

--- core.el
+++ core.el
@@ -2146,7 +2146,8 @@ spaces on either side of the point if so. Resorts to
     (bind-map-for-mode-inherit ivan/with-editor-leader-map ivan/leader-map
       :minor-modes (with-editor-mode)
       :bindings
-      ("w q RET" #'with-editor-finish))
+      ("w q RET" #'with-editor-finish)
+      ("c q RET" #'with-editor-cancel))
     ))
 
 (use-package magit
