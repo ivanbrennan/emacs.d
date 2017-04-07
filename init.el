141dc8a73f9f5c2728d37d70aa7f0e8de69e7fb4
ivanbrennan <ivan.brennan@gmail.com>
2017-04-06 23:32:52 -0400

    a bit of commentary

--- init.el
+++ init.el
@@ -1,15 +1,16 @@
-(let ((gc-cons-threshold 339430400)
-      (gc-cons-percentage 0.6)
-      (file-name-handler-alist nil))
+(let ((gc-cons-threshold 339430400)  ; Temporarily raise GC thresholds
+      (gc-cons-percentage 0.6)       ; and disable special I/O handlers
+      (file-name-handler-alist nil)) ; to optimize startup performance.
 
-  ;; blank slate
-  (setq inhibit-startup-echo-area-message "ivan")
+  ;; To suppress the startup messages in the echo area, this setting
+  ;; needs to occupy its own setq expression in your init file.
   ;; If your init file is byte-compiled, use the following form instead:
   ;;  (eval \\='(setq inhibit-startup-echo-area-message \"YOUR-USER-NAME\"))
+  (setq inhibit-startup-echo-area-message "ivan")
+
   (setq inhibit-startup-screen t
         initial-scratch-message nil)
 
-  ;; clean screen
   (menu-bar-mode   0)
   (tool-bar-mode   0)
   (when (display-graphic-p)
@@ -27,4 +28,8 @@
     (unless (server-running-p)
       (server-start))))
 
+;; Set a GC threshold greater than the default,
+;; but less than the value used during startup.
+;; The aim is to strike a balance between the
+;; frequency and duration of GC interruptions.
 (setq gc-cons-threshold 100000000)
