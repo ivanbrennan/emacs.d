ae561e87e0a8a89405686fc8a7ebf9ee24b2c337
ivanbrennan <ivan.brennan@gmail.com>
2017-04-17 00:31:19 -0400

    refine core-persistence

--- core/core-persistence.el
+++ core/core-persistence.el
@@ -10,44 +10,26 @@
   (eval-when-compile (ivan-cache-file "backups/"))
   "Storage area for backup files.")
 
-(defconst ivan-ido-persistency-file
-  (eval-when-compile (ivan-cache-file "ido.last"))
-  "File where ido state is persisted.")
-
-(defconst ivan-save-place-file
-  (eval-when-compile (ivan-cache-file "saveplace"))
-  "File where save-place-alist is persisted.")
-
-(defconst ivan-savehist-file
-  (eval-when-compile (ivan-cache-file "savehist"))
-  "File where minibuffer history is persisted.")
-
-(defconst ivan-tramp-persistency-file
-  (eval-when-compile (ivan-cache-file "tramp"))
-  "File which keeps connection history for Tramp connections.")
-
 (mkdir_p ivan-auto-save-directory)
 
 (setq auto-save-file-name-transforms `((".*" ,ivan-auto-save-directory 'uniquify))
       auto-save-list-file-prefix     ivan-auto-save-list-file-prefix
-
-      backup-by-copying      t
-      backup-directory-alist `(("." . ,ivan-backup-directory))
-
-      ido-save-directory-list-file ivan-ido-persistency-file)
+      backup-by-copying              t
+      backup-directory-alist         `(("." . ,ivan-backup-directory))
+      ido-save-directory-list-file   `(eval-when-compile (ivan-cache-file "ido.last")))
 
 (with-eval-after-load 'tramp-cache
   (eval-when-compile (defvar tramp-persistency-file-name))
-  (setq tramp-persistency-file-name ivan-tramp-persistency-file))
+  (setq tramp-persistency-file-name (eval-when-compile (ivan-cache-file "tramp"))))
 
 (use-package saveplace
   :config
-  (setq save-place-file ivan-save-place-file)
+  (setq save-place-file (eval-when-compile (ivan-cache-file "saveplace")))
   (save-place-mode +1))
 
 (use-package savehist
   :config
-  (setq savehist-file                 ivan-savehist-file
+  (setq savehist-file                 (eval-when-compile (ivan-cache-file "savehist"))
         savehist-autosave-interval    60
         savehist-additional-variables '(extended-command-history
                                         global-mark-ring
