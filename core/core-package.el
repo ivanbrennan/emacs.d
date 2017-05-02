0ab9e3f01d96d957ff5947b50efc8ebabe990a1d
ivanbrennan <ivan.brennan@gmail.com>
2017-05-02 07:19:48 -0400

    core-package fix alignment

--- core/core-package.el
+++ core/core-package.el
@@ -17,7 +17,7 @@
       gnutls-trustfiles   `(,ivan-trustfile)
       gnutls-verify-error t
 
-      package-archives '(("gnu"          . "https://elpa.gnu.org/packages/")
+      package-archives '(("gnu"         . "https://elpa.gnu.org/packages/")
                         ("melpa-stable" . "https://stable.melpa.org/packages/")
                         ("melpa"        . "https://melpa.org/packages/")
                         ("marmalade"    . "https://ojab.ru/marmalade/"))
