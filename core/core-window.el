16b70ce5661c6b3f696f9193c4c9e779cfb7cdb4
ivanbrennan <ivan.brennan@gmail.com>
2017-05-01 23:25:14 -0400

    core-window: start extracting reusable variables

--- core/core-window.el
+++ core/core-window.el
@@ -8,40 +8,44 @@
 
 ;; splits, frames, windows
 (setq split-width-threshold 130)
-(setq
- display-buffer-alist
- `(
-   ;; Put search results in bottom side-window of the current frame.
-   (,(rx bos
-         (or
-          (and "*ag " (1+ not-newline) "*")
-          "*ggtags-global*"
-          "*Help*"
-          "*Apropos*"
-          "*rake-compilation*"
-          "*Pp Eval Output*"
-          )
-         eos)
-    (display-buffer-reuse-window
-     display-buffer-in-side-window)
-    (reusable-frames)
-    (side . bottom)
-    )
-   ;; Put test results in reusable window/frame if one is visible,
-   ;; otherwise put them in bottom side-window.
-   (,(rx bos
-         (or
-          "*rspec-compilation*"
-          "*ert*"
-          "*quickrun*"
-          )
-         eos)
-    (display-buffer-reuse-window
-     display-buffer-in-side-window)
-    (reusable-frames . visible)
-    (inhibit-switch-frame . t)
-    (side . bottom)
-    )
-   ))
+
+(defvar ivan-bottom-buffer-patterns
+  '((and "*ag " (1+ not-newline) "*")
+    "*ggtags-global*"
+    "*Help*"
+    "*Apropos*"
+    "*rake-compilation*"
+    "*Pp Eval Output*"))
+
+(defvar ivan-bottom-or-reusable-window-patterns
+  '("*rspec-compilation*"
+    "*ert*"
+    "*quickrun*"))
+
+(defmacro ivan-bottom-buffer-regex ()
+  `(rx bos (or ,@ivan-bottom-buffer-patterns) eos))
+
+(defmacro ivan-bottom-or-reusable-window-regex ()
+  `(rx bos (or ,@ivan-bottom-or-reusable-window-patterns) eos))
+
+(setq display-buffer-alist
+      `(
+        ;; Put search results in bottom side-window of the current frame.
+        (,(ivan-bottom-buffer-regex)
+         (display-buffer-reuse-window
+          display-buffer-in-side-window)
+         (reusable-frames)
+         (side . bottom)
+         )
+        ;; Put test results in reusable window/frame if one is visible,
+        ;; otherwise put them in bottom side-window.
+        (,(ivan-bottom-or-reusable-window-regex)
+         (display-buffer-reuse-window
+          display-buffer-in-side-window)
+         (reusable-frames . visible)
+         (inhibit-switch-frame . t)
+         (side . bottom)
+         )
+        ))
 
 (provide 'core-window)
