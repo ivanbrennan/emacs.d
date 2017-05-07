cc7ca3cbd96aad13c4d1bbbc9bed1ea03f889e8a
ivanbrennan <ivan.brennan@gmail.com>
2017-05-07 17:11:44 -0400

    evil: update 20170323.1140 -> 20170419.37
    Very small update.

---
+++ packages/evil-20170419.37/evil-autoloads.el
@@ -0,0 +1,40 @@
+;;; evil-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "evil-core" "../../../../../../.emacs.d/packages/evil-20170419.37/evil-core.el"
+;;;;;;  "3df1e5fb6ee91601100fbf75d448647b")
+;;; Generated autoloads from ../../../../../../.emacs.d/packages/evil-20170419.37/evil-core.el
+ (autoload 'evil-mode "evil")
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../../.emacs.d/packages/evil-20170419.37/evil-autoloads.el"
+;;;;;;  "../../../../../../.emacs.d/packages/evil-20170419.37/evil-command-window.el"
+;;;;;;  "../../../../../../.emacs.d/packages/evil-20170419.37/evil-commands.el"
+;;;;;;  "../../../../../../.emacs.d/packages/evil-20170419.37/evil-common.el"
+;;;;;;  "../../../../../../.emacs.d/packages/evil-20170419.37/evil-core.el"
+;;;;;;  "../../../../../../.emacs.d/packages/evil-20170419.37/evil-digraphs.el"
+;;;;;;  "../../../../../../.emacs.d/packages/evil-20170419.37/evil-ex.el"
+;;;;;;  "../../../../../../.emacs.d/packages/evil-20170419.37/evil-integration.el"
+;;;;;;  "../../../../../../.emacs.d/packages/evil-20170419.37/evil-jumps.el"
+;;;;;;  "../../../../../../.emacs.d/packages/evil-20170419.37/evil-macros.el"
+;;;;;;  "../../../../../../.emacs.d/packages/evil-20170419.37/evil-maps.el"
+;;;;;;  "../../../../../../.emacs.d/packages/evil-20170419.37/evil-pkg.el"
+;;;;;;  "../../../../../../.emacs.d/packages/evil-20170419.37/evil-repeat.el"
+;;;;;;  "../../../../../../.emacs.d/packages/evil-20170419.37/evil-search.el"
+;;;;;;  "../../../../../../.emacs.d/packages/evil-20170419.37/evil-states.el"
+;;;;;;  "../../../../../../.emacs.d/packages/evil-20170419.37/evil-types.el"
+;;;;;;  "../../../../../../.emacs.d/packages/evil-20170419.37/evil-vars.el"
+;;;;;;  "../../../../../../.emacs.d/packages/evil-20170419.37/evil.el")
+;;;;;;  (22799 36206 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; evil-autoloads.el ends here
