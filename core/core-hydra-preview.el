455f8c40c53e54a0485bef1cc16e2fe038eaf19a
ivanbrennan <ivan.brennan@gmail.com>
2017-05-07 13:26:53 -0400

    hydra-preview: finishing touches
    Polish the function names, interfaces, etc.

--- core/core-hydra-preview.el
+++ core/core-hydra-preview.el
@@ -27,66 +27,64 @@
 (defun dired-preview-current ()
   "Preview the current file in another window."
   (interactive)
-  (dired-preview-assert-mode)
-  (let ((win  (dired-preview-find-window 'or-create))
+  (dired-preview--assert-mode)
+  (let ((win (dired-preview--find-window 'or-create))
         (file (dired-get-file-for-visit)))
-    (dired-preview-clean win :keep-buffer (current-buffer))
+    (dired-preview--maybe-kill-window-buffer win)
     (with-selected-window win
-      (dired-preview-file file))))
+      (dired-preview--view-file file))))
 
-(defun dired-preview-assert-mode ()
+(defun dired-preview-quit ()
+  "Quit previewing from Dired, clean up preview buffer and window."
+  (interactive)
+  (dired-preview--assert-mode)
+  (let ((win (dired-preview--find-window)))
+    (dired-preview--maybe-kill-window-buffer win)
+    (dired-preview--maybe-delete-window win)))
+
+(defun ivan-view-quit ()
+  "Quit view-mode, clean up preview buffer and window."
+  (interactive)
+  (let ((win (selected-window)))
+    (dired-preview--maybe-kill-window-buffer win 'can-kill-current)
+    (dired-preview--maybe-delete-window win)))
+
+(defun dired-preview--assert-mode ()
   (unless (eq 'dired-mode major-mode)
-    (user-error "dired-preview is designed for use from dired")))
+    (user-error "dired-preview is made for use with dired")))
 
-(defun dired-preview-find-window (&optional or-create)
+(defun dired-preview--find-window (&optional or-create)
   (if (window-parent)
       (next-window)
-    (when or-create (dired-preview-create-window))))
+    (when or-create (dired-preview--create-window))))
 
-(defun dired-preview-create-window ()
+(defun dired-preview--create-window ()
   (let ((win (split-window-sensibly)))
     (unless win (error "Failed to create preview window"))
     (set-window-parameter win 'created-for-preview t)
     win))
 
-(defun dired-preview-clean (win &rest args)
-  (let ((quit-preview (plist-get args :quit-preview))
-        (keep-buffer (plist-get args :keep-buffer))
-        (buf (window-buffer win)))
-    (when (and (not (eq keep-buffer buf))
-               (dired-preview-disposable? buf))
-      (kill-buffer buf))
-    (when (and quit-preview
-               (window-parameter win 'created-for-preview))
-      (delete-window win))))
+(defun dired-preview--maybe-kill-window-buffer (win &optional can-kill-current)
+  (let ((buf (window-buffer win)))
+    (when (dired-preview--can-kill-buffer? buf can-kill-current)
+      (kill-buffer buf))))
 
-(defun dired-preview-disposable? (buf)
-  (and (not (buffer-modified-p buf))
+(defun dired-preview--can-kill-buffer? (buf can-kill-current)
+  (and (or can-kill-current (not (eq (current-buffer) buf)))
+       (not (buffer-modified-p buf))
        (or (buffer-local-value 'view-mode buf)
            (eq 'dired-mode (buffer-local-value 'major-mode buf)))))
 
-(defun dired-preview-created-window? (win)
-  (window-parameter win 'created-for-preview))
+(defun dired-preview--maybe-delete-window (win)
+  (when (window-parameter win 'created-for-preview)
+    (delete-window win)))
 
-(defun dired-preview-file (file)
+(defun dired-preview--view-file (file)
   (let ((buf (get-file-buffer file)))
     (if buf
         (switch-to-buffer buf)
       (let ((inhibit-message t))
         (view-file file)))))
-
-(defun dired-preview-quit ()
-  "Quit the preview buffer, and possibly its window, from dired."
-  (interactive)
-  (dired-preview-assert-mode)
-  (let ((win (dired-preview-find-window)))
-    (dired-preview-clean win :keep-buffer (current-buffer)
-                             :quit-preview t)))
-
-(defun ivan-view-quit ()
-  "Quit view-mode and clean up buffer/window."
-  (interactive)
-  (dired-preview-clean (selected-window) :quit-preview t))
 
 (with-eval-after-load 'view
   (define-key view-mode-map "q" #'ivan-view-quit))
