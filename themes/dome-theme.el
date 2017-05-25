f556eb6faba55df18e7b50dde69bfee9dfd2feb8
ivanbrennan <ivan.brennan@gmail.com>
2017-05-24 21:32:10 -0400

    dome-theme: improve highlight colors

--- themes/dome-theme.el
+++ themes/dome-theme.el
@@ -113,7 +113,7 @@
      `(default                ((,c (:background ,bg-d :foreground ,fg))))
      `(fringe                 ((,c (:inherit default :foreground ,comments))))
      `(region                 ((,c (:background ,region))))
-     `(highlight              ((,c (:background ,region :foreground ,black))))
+     `(highlight              ((,c (:background ,region))))
      `(hl-line                ((,c (:background ,bg))))
      `(cursor                 ((,c (:background ,fg))))
      `(link                   ((,c (:foreground "#65B1EC" :underline t))))
