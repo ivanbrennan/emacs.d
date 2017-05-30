1495a2c45fc5671eae83799ce479b38f1bdf9605
ivanbrennan <ivan.brennan@gmail.com>
2017-05-30 09:52:59 -0400

    fix sh-indentation

--- core.el
+++ core.el
@@ -1937,7 +1937,9 @@ spaces on either side of the point if so. Resorts to
 (use-package sh-script
   :mode ("/\\.?bash\\(/.*\\|rc\\|_profile\\|\\)$" . sh-mode)
   :init (add-hook 'sh-mode-hook (lambda () (setq mode-name "shell")))
-  :config (setq sh-basic-offset 2))
+  :config
+  (setq sh-basic-offset 2
+        sh-indentation  sh-basic-offset))
 
 (use-package face-remap
   :diminish text-scale-mode
